4c45a1e5e87012b171b3bb9a14d9f520bd12dcfa
Antonio-ckl <antonio.cklebis@gmail.com>
2025-04-01 13:20:13 -0300

    Login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+
 use App\Livewire\Cliente\Create;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/create/cliente', Create::class);
+
+Route::get('/login', Login::class)->name('login');
